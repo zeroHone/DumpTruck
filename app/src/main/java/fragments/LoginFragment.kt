0b90eadfa92a0cfa90a2a9ddb98d326d748f0519
zeroHone <hosein.habibifar@gmail.com>
2021-12-28 16:01:24 +0330

    12/28/2021-15:22

---
+++ app/src/main/java/fragments/LoginFragment.kt
@@ -0,0 +1,44 @@
+package fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.example.dumptruck.R
+import com.google.android.material.textfield.TextInputEditText
+
+
+class LoginFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        view.findViewById<Button>(R.id.btnLogin).setOnClickListener {
+             val user =view.findViewById<TextInputEditText>(R.id.txtUser).text.toString()
+            val password = view.findViewById<TextInputEditText>(R.id.txtPass).text.toString()
+            if(loginAttempt(user, password)){
+                activity?.supportFragmentManager?.beginTransaction().apply {
+
+                }
+            }
+        }
+    return view
+    }
+
+    fun loginAttempt(user : String , pass: String):Boolean{
+        return pass.length == 8
+    }
+
+}
