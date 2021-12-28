51ebdaa8d226d750659dce4346888039c1f99b9b
zeroHone <hosein.habibifar@gmail.com>
2021-12-28 13:15:17 +0330

    12/28/2021-13:14

--- app/src/main/java/com/example/dumptruck/MainActivity.kt
+++ app/src/main/java/com/example/dumptruck/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.dumptruck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var toggle : ActionBarDrawerToggle
+    var myDummy : Int =0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toggle = ActionBarDrawerToggle(this,findViewById(R.id.baseDrawer),R.string.open,R.string.close)
+        findViewById<DrawerLayout>(R.id.baseDrawer).addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        findViewById<NavigationView>(R.id.navDrawer).setNavigationItemSelectedListener{
+
+            when(it.itemId){
+                R.id.menuItemLiveData ->myDummy=1
+                R.id.menuItemLiveData -> myDummy=2
+
+
+            }
+            true
+        }
+
+
+
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
